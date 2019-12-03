a6428c87388354963512395217655945adc5fe55
Ankita Jain <ankita.jain12342@gmail.com>
2019-12-03 15:29:31 -0500

    workingh on adapter

--- app/src/main/java/com/example/recyclerviewdatabinding/RecyclerAdapter.java
+++ app/src/main/java/com/example/recyclerviewdatabinding/RecyclerAdapter.java
@@ -1,4 +1,54 @@
 package com.example.recyclerviewdatabinding;
 
-public class RecyclerAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.recyclerviewdatabinding.databinding.ItemLayoutBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder>
+{
+    private Context context;
+    private List<Users> users=new ArrayList<>();
+
+    public RecyclerAdapter(Context context, List<Users> users)
+    {
+        this.context = context;
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder
+    {
+
+        ItemLayoutBinding itemLayoutBinding;
+
+
+        public MyViewHolder(@NonNull ItemLayoutBinding itemView) {
+            super(itemView.getRoot());
+            itemLayoutBinding=itemView;
+        }
+    }
 }
